7718f419bbe1e78082f94eee0ce91b68fc68db4d
samuelehis <ehis.samuel@outlook.com>
2022-04-20 23:49:08 -0700

    update puts.c and printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,9 @@
 
 
 
-/**
- *
+/*
  *   main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *    Return: 0 if exited properly, non-zero otherwise
- *
+ *   Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
