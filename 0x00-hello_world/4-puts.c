a6252cb60ec34375f7df8485b32aa45de18991c4
samuelehis <ehis.samuel@outlook.com>
2022-04-21 23:31:30 -0700

    update intel.c and quote.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /*
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
@@ -8,7 +7,6 @@
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
